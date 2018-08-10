dbb1b08b607624b78a30f1449224aea882521302
cgoez <goez2001@gmail.com>
2018-08-09 22:55:48 -0700

    Styling

--- drum-machine/src/components/DrumRack/DrumRack.js
+++ drum-machine/src/components/DrumRack/DrumRack.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Drums from "../Drums/Drums";
 
- import './DrumRack.css'
+import "./DrumRack.css";
 
 // import sounds
 import boom from "../../sounds/boom.wav";
@@ -17,70 +17,79 @@ import tom from "../../sounds/tom.wav";
 export default class DrumRack extends Component {
   render() {
     return (
-      <div className="drumracks">
-        <Drums
-          className="drumpad boom"
-          sound="boom"
-          keycode={81}
-          keyLetter="q"
-          source={boom}
-        />
-        <Drums
-          className="drumpad clap"
-          sound="clap"
-          keycode={87}
-          keyLetter="w"
-          source={clap}
-        />
-        <Drums
-          className="drumpad hihat"
-          sound="hihat"
-          keycode={69}
-          keyLetter="e"
-          source={hihat}
-        />
-        <Drums
-          className="drumpad kick"
-          sound="kick"
-          keycode={65}
-          keyLetter="a"
-          source={kick}
-        />
-        <Drums
-          className="drumpad openhat"
-          sound="openhat"
-          keycode={83}
-          keyLetter="s"
-          source={openhat}
-        />
-        <Drums
-          className="drumpad ride"
-          sound="ride"
-          keycode={68}
-          keyLetter="d"
-          source={ride}
-        />
-        <Drums
-          className="drumpad snare"
-          sound="snare"
-          keycode={90}
-          keyLetter="z"
-          source={snare}
-        />
-        <Drums
-          className="drumpad tink"
-          sound="tink"
-          keycode={88}
-          keyLetter="x"
-          source={tink}
-        />
-        <Drums
-          className="drumpad tom"
-          sound="tom"
-          keycode={67}
-          keyLetter="c"
-          source={tom}
-        />
+      <div className="drumrack">
+      <Drums
+            className="boom"
+            sound="boom"
+            keycode={81}
+            keyLetter="q"
+            source={boom}
+          />
+          <Drums
+            className="clap"
+            sound="clap"
+            keycode={87}
+            keyLetter="w"
+            source={clap}
+          />
+          <Drums
+            className="hihat"
+            sound="hihat"
+            keycode={69}
+            keyLetter="e"
+            source={hihat}
+          />
+          <Drums
+            className="kick"
+            sound="kick"
+            keycode={65}
+            keyLetter="a"
+            source={kick}
+          />
+          <Drums
+            className="openhat"
+            sound="openhat"
+            keycode={83}
+            keyLetter="s"
+            source={openhat}
+          />
+          <Drums
+            className="drumpad ride"
+            sound="ride"
+            keycode={68}
+            keyLetter="d"
+            source={ride}
+          />
+          <Drums
+            className="drumpad snare"
+            sound="snare"
+            keycode={90}
+            keyLetter="z"
+            source={snare}
+          />
+          <Drums
+            className="drumpad tink"
+            sound="tink"
+            keycode={88}
+            keyLetter="x"
+            source={tink}
+          />
+          <Drums
+            className="drumpad tom"
+            sound="tom"
+            keycode={67}
+            keyLetter="c"
+            source={tom}
+          />
+        <div className="rowOne">
+          
+        </div>
+        <div className="rowTwo">
+          
+        </div>
+        <div className="rowThree">
+          
+        </div>
       </div>
     );
   }
